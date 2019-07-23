68540cea8c3abbff6634142669c7a6b828aeae9b
Lobarr <jesulobaegunjobi@hotmail.com>
2019-07-22 20:45:27 -0400

    refactor: cleaned up

--- api/util/api.py
+++ api/util/api.py
@@ -1,5 +1,6 @@
 import bson
 import aiohttp
+import asyncio
 import json
 import requests
 from typing import Optional
@@ -8,28 +9,20 @@ from motor.motor_asyncio import AsyncIOMotorClient
 class Api:
   @staticmethod
   async def call(method, url, params=None, data=None, json=None, cookies=None, headers=None, auth=None):
-    async with aiohttp.request(method=method, url=url, params=params, data=data, json=json, cookies=cookies, headers=headers, auth=auth) as resp:
-      return resp
-    
+    return await aiohttp.request(method=method, url=url, params=params, data=data, json=json, cookies=cookies, headers=headers, auth=auth)
+  
   @staticmethod
-  async def batch(requests):
+  def batch(requests):
     results = []
-    for i in range(len(headers)):
-        res = requests.post(urls[i], headers=headers[i], data=json.dumps(bodies[i]))
+    for request in requests:
+        res = asyncio.run(Api.call(request['method'], request['url'], request['params'], request['data'], request['json'], request['cookies'], request['headers'], request['auth']))
         results.append(res)
     return results
 
   @staticmethod
-  async def batch_async(headers, bodies, urls):
-    """
-    Should accept 3 lists of parameters (list of headers, list of bodies, list of urls) to be run
-    """
+  async def batch_async(requests):
     results = []
-    for i in range(len(headers)):
-        res = await ApiUtil.single(headers[i], bodies[i], urls[i])
-        results.append(res)
+    for request in requests:
+      res = await Api.call(request['method'], request['url'], request['params'], request['data'], request['json'], request['cookies'], request['headers'], request['auth'])
+      results.append(res)
     return results
-
-  #things not done:
-  # currently synchronous_batch runs with requests but i am not sure thats the right way to make it synchronous.
-  #Should include headers, body, method, and redirect properties from request?
