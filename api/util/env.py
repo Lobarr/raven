0b1afbdff015e24949cde76b74ac8f55a7d332bf
Lobarr <jesulobaegunjobi@hotmail.com>
2019-07-28 19:43:45 -0400

    chore: added admin user and pass to envs

--- api/util/env.py
+++ api/util/env.py
@@ -5,3 +5,5 @@ ENV = os.getenv('ENV') or 'dev'
 DB = os.getenv('DB')
 JWT_SECRET = os.getenv('JWT_SECRET')
 REDIS = os.getenv('REDIS')
+RAVEN_ADMIN_USER = os.getenv('RAVEN_ADMIN_USER')
+RAVEN_ADMIN_PASS = os.getenv('RAVEN_ADMIN_PASS')
