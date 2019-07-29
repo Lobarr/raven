dda7d7ec044575837650bb2ee71f8960dcf2a401
Lobarr <jesulobaegunjobi@hotmail.com>
2019-07-28 21:36:42 -0400

    fix: added func return type

--- api/util/password.py
+++ api/util/password.py
@@ -1,16 +1,11 @@
 import bcrypt
+from password_strength.policy import PasswordPolicy, PasswordStats
 
 class Password:
   @staticmethod
-  def hash(password: str):
+  def hash(password: str) -> str:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(14)).decode('utf-8')
   
   @staticmethod
-  def validate(password: str, hash: str):
+  def validate(password: str, hash: str) -> bool:
     return bcrypt.checkpw(password.encode('utf-8'), hash.encode('utf-8'))
-
-
-
-
-
-
