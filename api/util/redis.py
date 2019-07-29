4715785722e4b1f7e878042ac5afd7bd5d4fe0eb
Lobarr <jesulobaegunjobi@hotmail.com>
2019-07-28 21:36:56 -0400

    style: clean up

--- api/util/redis.py
+++ api/util/redis.py
@@ -4,25 +4,24 @@ from api.util import Bson, DB
 import base64
 
 class Redis:
-    
-    @staticmethod
-    async def fetch_members(set, db):
-        keys = await db.smembers(set)
-        results = []
-        for key in keys:
-            key_ = Bson.to_json(key)['$binary']
-            val = await db.get(base64.b64decode(key_))
-            if val is None:
-                db.srem(set, base64.b64decode(key_))
-                continue
-            val_ = json.loads(base64.b64decode(Bson.to_json(val)['$binary']))
-            results.append(val_)
-        return results
+  @staticmethod
+  async def fetch_members(set, db):
+    keys = await db.smembers(set)
+    results = []
+    for key in keys:
+      _key = Bson.to_json(key)['$binary']
+      val = await db.get(base64.b64decode(_key))
+      if val is None:
+        db.srem(set, base64.b64decode(_key))
+        continue
+      _val = json.loads(base64.b64decode(Bson.to_json(val)['$binary']))
+      results.append(_val)
+    return results
 
-    @staticmethod
-    async def format_response(response):
-        response = Bson.to_json(response)
-        values = []
-        for item in response:
-            values.append(item)
-        return values
+  @staticmethod
+  async def format_response(response):
+    response = Bson.to_json(response)
+    values = []
+    for item in response:
+        values.append(item)
+    return values
