2b456765002348cd0a9bee46a5baa93377ff8e53
Lobarr <jesulobaegunjobi@hotmail.com>
2019-08-07 14:30:02 -0400

    fix: implemented response encoding

--- api/util/redis.py
+++ api/util/redis.py
@@ -1,22 +1,13 @@
 import json
+import base64
 from bson import json_util
 from api.util import Bson, DB
-import base64
+from aioredis import Redis
 
 class Redis:
   @staticmethod
-  async def fetch_members(set, db):
-    keys = await db.smembers(set)
-    results = []
-    for key in keys:
-      _key = Bson.to_json(key)['$binary']
-      val = await db.get(base64.b64decode(_key))
-      if val is None:
-        db.srem(set, base64.b64decode(_key))
-        continue
-      _val = json.loads(base64.b64decode(Bson.to_json(val)['$binary']))
-      results.append(_val)
-    return results
+  async def fetch_members(key, db: Redis):
+    return await db.smembers(key, encoding='utf-8')
 
   @staticmethod
   async def format_response(response):
