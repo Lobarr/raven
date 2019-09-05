573d96dd183a69046a2cda5b89b2c7d73445aec6
Lobarr <jesulobaegunjobi@hotmail.com>
2019-09-05 10:23:59 -0700

    tests: updated tests

--- tests/circuit_breaker/test_schema.py
+++ tests/circuit_breaker/test_schema.py
@@ -2,8 +2,8 @@ from expects import expect, have_keys, equal
 from api.circuit_breaker import circuit_breaker_schema
 
 def test_circuit_breaker_schema():
-  expect(circuit_breaker_schema).to(have_keys('_id', 'status', 'service_id', 'cooldown', 'status_codes', 'method', 'path', 'threshold'))
-  for prop in ['_id', 'status', 'service_id', 'method', 'path']:
+  expect(circuit_breaker_schema).to(have_keys('_id', 'status', 'service_id', 'cooldown', 'status_codes', 'method', 'threshold'))
+  for prop in ['_id', 'status', 'service_id', 'method']:
     expect(circuit_breaker_schema[prop]['type']).to(equal('string'))
 
   for prop in ['cooldown', 'period', 'tripped_count']:
