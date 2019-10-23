33fe02ca0d2f7e00021462c4d2e387f0be4c4135
Lobarr <jesulobaegunjobi@hotmail.com>
2019-10-23 15:03:43 -0700

    feat: progress on typescript integration

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './pages';
+import * as serviceWorker from './utils/serviceWorker';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
---
+++ client/src/pages/app/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>testing testing testing</h1>
+    </div>
+  );
+};
+
+export default App;
